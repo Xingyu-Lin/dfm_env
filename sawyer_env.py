ac16d272e2517139a1f1d7c03097cef31fdff5c7
Xingyu-Lin <abclin6693@gmail.com>
2018-12-11 19:15:16 -0500

    small changes

--- sawyer_env.py
+++ sawyer_env.py
@@ -100,7 +100,7 @@ class SawyerEnv(Base, gym.utils.EzPickle):
 
     def _set_action(self, ctrl):
         # todo ADD argument for mocap constrained 2d
-        ctrl /= 20
+        ctrl /= self.n_substeps
         # ctrl *=0
         # print('mocap:' , self.physics.data.mocap_pos[0], self.physics.data.mocap_quat[0])
         # print('gripper', self.physics.data.xpos[26], self.physics.data.xquat[26])
